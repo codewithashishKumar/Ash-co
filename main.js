36a41162c8707f55d192aa0ac8100ddbcf2b1ed3
codewithashishKumar <himanashish446@gmail.com>
2026-09-09 00:07:39 +0530

    loader aded

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ const products = [
     reviews: 72,
     badge: "Limited",
     image:
-      "https://images.unsplash.com/photo-1596755389378-c31d21fd1273?auto=format&fit=crop&w=900&q=85",
+      "https://images.unsplash.com/photo-1644483878413-1bdfe8d1180c?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     description:
       "A versatile textured overshirt designed for layering through changing seasons.",
   },
